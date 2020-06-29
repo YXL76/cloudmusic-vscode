cfe38aa0307ed397a875ca591205b3c58da045c1
YXL <chenxin.lan.76@gmail.com>
2020-06-29 12:56:14 +0800

    ✨feat(utli/utli): solveSongItem now can solve multi artists

--- src/util/util.ts
+++ src/util/util.ts
@@ -2,10 +2,15 @@ import { QueueItem, songsItem } from "../constant/type";
 
 export function solveSongItem(item: songsItem): QueueItem {
   const { name, id, alia, ar } = item;
+  let arNames = [];
+  for (const i of ar) {
+    arNames.push(i.name);
+  }
+  const arName = arNames.join("/");
   return {
     name,
     id,
     alia: alia ? alia[0] : "",
-    arName: ar[0].name,
+    arName,
   };
 }
