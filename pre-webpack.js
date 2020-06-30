8266b54cf649853d57e175a3bb3fc8fb6cafa747
YXL <chenxin.lan.76@gmail.com>
2020-06-30 10:30:58 +0800

    🐞fix(webpack): fix Critical dependency

---
+++ pre-webpack.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const path = require("path");
+
+const dir = path.join(".", "node_modules", "NeteaseCloudMusicApi");
+
+let data = [];
+
+fs.readdirSync(path.join(dir, "module"))
+  .reverse()
+  .forEach((file) => {
+    if (!file.endsWith(".js")) {
+      return;
+    }
+    const name = file.split(".").shift();
+    data.push(`
+    let ${name} = require("./module/${name}")
+    obj["${name}"] = function (data) {
+      if (typeof data.cookie === "string") {
+        data.cookie = cookieToJson(data.cookie);
+      }
+      return ${name}(
+        {
+          ...data,
+          cookie: data.cookie ? data.cookie : {},
+        },
+        request
+      );
+    };`);
+  });
+
+fs.writeFileSync(
+  path.join(dir, "main.js"),
+  `
+  const fs = require('fs')
+  const path = require('path')
+  const request = require('./util/request')
+  const { cookieToJson } = require('./util/index')
+
+  let obj = {}
+
+  ${data.join("\n")}
+  
+  module.exports = obj
+  `
+);
