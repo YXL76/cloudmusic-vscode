e157418902a85dd769abf91c5c9679e9b7b0095c
YXL <chenxin.lan.76@gmail.com>
2020-08-12 01:41:42 +0800

    🐞fix(page): fix error and modify style

--- page/ts/userMusicRanking.ts
+++ page/ts/userMusicRanking.ts
@@ -5,27 +5,29 @@ window.addEventListener("message", (event) => {
   const max = songs[0].count;
   const root = document.getElementById("list");
 
-  let i = 0;
-  for (const { count, name, alia } of songs) {
-    const item = document.createElement("div");
-    item.className = "list__item";
+  if (root) {
+    let i = 0;
+    for (const { count, name, alia } of songs) {
+      const item = document.createElement("div");
+      item.className = "list__item";
 
-    const left = document.createElement("div");
-    left.className = "list__item-left";
-    left.innerHTML = `${++i}`;
-    item.appendChild(left);
+      const left = document.createElement("div");
+      left.className = "list__item-left";
+      left.innerHTML = `${++i}`;
+      item.appendChild(left);
 
-    const middle = document.createElement("div");
-    middle.className = "list__item-middle";
-    middle.innerHTML = `${name} ${alia ? `(${alia})` : ""}`;
-    item.appendChild(middle);
+      const middle = document.createElement("div");
+      middle.className = "list__item-middle";
+      middle.innerHTML = `${name} ${alia[0] ? `(${alia[0]})` : ""}`;
+      item.appendChild(middle);
 
-    const right = document.createElement("div");
-    right.className = "list__item-right";
-    right.innerHTML = `${count}`;
-    right.style.width = `${(count * 54) / max}vw`;
-    item.appendChild(right);
+      const right = document.createElement("div");
+      right.className = "list__item-right";
+      right.innerHTML = `${count}`;
+      right.style.width = `${(count * 54) / max}vw`;
+      item.appendChild(right);
 
-    root.appendChild(item);
+      root.appendChild(item);
+    }
   }
 });
