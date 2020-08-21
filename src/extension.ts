159817e9cba128fc6ba18e2c43132d97e8195b87
YXL <chenxin.lan.76@gmail.com>
2020-08-21 17:12:06 +0800

    ✨feat(activate): add progress

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,39 @@
+import { ExtensionContext, ProgressLocation, window } from "vscode";
 import { LyricCache, MusicCache, player } from "./util";
-import { ExtensionContext } from "vscode";
 import { TMP_DIR } from "./constant";
 import { steps } from "./activate";
 import del = require("del");
 
 export function activate(context: ExtensionContext): void {
-  for (const progress of steps) {
-    progress(context);
-  }
+  window.withProgress(
+    {
+      location: ProgressLocation.Notification,
+      title: "Cloudmusic initialization",
+      cancellable: true,
+    },
+    (progress) => {
+      let percentage = 0;
+      const unit = Math.floor(100 / steps.length);
+
+      for (const step of steps) {
+        progress.report({
+          increment: percentage,
+          message: "Initializing...",
+        });
+        step(context);
+        percentage += unit;
+      }
+
+      progress.report({
+        increment: percentage,
+        message: "Initialization ended",
+      });
+
+      return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+      });
+    }
+  );
 }
 
 export function deactivate(): void {
