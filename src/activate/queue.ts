26b729647867fa9ddf82fbd33f27cdc4332d42dc
YXL <chenxin.lan.76@gmail.com>
2020-08-27 21:32:14 +0800

    ✨feat(command): add `cloudmusic.sortQueue`

--- src/activate/queue.ts
+++ src/activate/queue.ts
@@ -1,11 +1,99 @@
+import { MultiStepInput, load, stop } from "../util";
 import { PersonalFm, lock } from "../state";
 import { QueueItemTreeItem, QueueProvider } from "../provider";
-import { commands, window } from "vscode";
-import { load, stop } from "../util";
+import { QuickPickItem, commands, window } from "vscode";
+import { ICON } from "../constant";
+import { i18n } from "../i18n";
 
 export async function initQueue(): Promise<void> {
   const queueProvider = QueueProvider.getInstance();
   window.registerTreeDataProvider("queue", queueProvider);
+
+  commands.registerCommand("cloudmusic.sortQueue", () => {
+    MultiStepInput.run((input: MultiStepInput) => pickType(input));
+
+    async function pickType(input: MultiStepInput) {
+      enum Type {
+        song,
+        album,
+        artist,
+      }
+      enum Order {
+        ascending,
+        descending,
+      }
+      interface T extends QuickPickItem {
+        type: Type;
+        order: Order;
+      }
+
+      const pick = await input.showQuickPick<T>({
+        title: i18n.word.account,
+        step: 1,
+        totalSteps: 1,
+        items: [
+          {
+            label: `${ICON.song} ${i18n.word.song}`,
+            description: i18n.word.ascending,
+            type: Type.song,
+            order: Order.ascending,
+          },
+          {
+            label: `${ICON.song} ${i18n.word.song}`,
+            description: i18n.word.descending,
+            type: Type.song,
+            order: Order.descending,
+          },
+          {
+            label: `${ICON.album} ${i18n.word.album}`,
+            description: i18n.word.ascending,
+            type: Type.album,
+            order: Order.ascending,
+          },
+          {
+            label: `${ICON.album} ${i18n.word.album}`,
+            description: i18n.word.descending,
+            type: Type.album,
+            order: Order.descending,
+          },
+          {
+            label: `${ICON.artist} ${i18n.word.artist}`,
+            description: i18n.word.ascending,
+            type: Type.artist,
+            order: Order.ascending,
+          },
+          {
+            label: `${ICON.artist} ${i18n.word.artist}`,
+            description: i18n.word.descending,
+            type: Type.artist,
+            order: Order.descending,
+          },
+        ],
+      });
+
+      const { songs } = QueueProvider;
+      stop();
+      await QueueProvider.refresh(async () => {
+        if (pick.type === Type.song) {
+          QueueProvider.songs = songs.sort((a, b) =>
+            a.item.name.localeCompare(b.item.name)
+          );
+        } else if (pick.type === Type.album) {
+          QueueProvider.songs = songs.sort((a, b) =>
+            a.item.al.name.localeCompare(b.item.al.name)
+          );
+        } else if (pick.type === Type.artist) {
+          QueueProvider.songs = songs.sort((a, b) =>
+            a.item.ar[0].name.localeCompare(b.item.ar[0].name)
+          );
+        }
+        if (pick.order === Order.descending) {
+          QueueProvider.songs.reverse();
+        }
+      });
+      return input.pop();
+    }
+  });
 
   commands.registerCommand("cloudmusic.clearQueue", () => {
     QueueProvider.refresh(async () => {
