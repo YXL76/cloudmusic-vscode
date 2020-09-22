a1c930af3aec219d74fbf5a2e8f413bc87926ad9
YXL <chenxin.lan.76@gmail.com>
2020-09-22 20:44:30 +0800

    🦄refactor(rootDIr): move `src` to `ext-src`

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const path = require("path");
 const config = {
   target: "node", // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 
-  entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  entry: "./ext-src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, "dist"),
