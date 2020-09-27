c19002cd6a8321eeab98ab2bde80082ddd17dfc3
YXL <chenxin.lan.76@gmail.com>
2020-09-27 12:47:49 +0800

    🐳chore(dependencies): use specify `NeteaseCloudMusicApi`

--- src/module.d.ts
+++ src/module.d.ts
@@ -5,3 +5,14 @@ declare module "NeteaseCloudMusicApi/util/index" {
 declare module "array-unsort" {
   export function unsortInplace<T>(items: T[]): T[];
 }
+
+declare module "cacahe" {
+  export interface CacheObject {
+    integrity: string;
+    key: string;
+    metadata?: any;
+    path: string;
+    time: number;
+    size: number;
+  }
+}
