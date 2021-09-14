8e0adf109af625827f74b311a6b8863a5a2e2f2c
YXL <chenxin.lan.76@gmail.com>
2021-09-14 21:45:21 +0800

    🐎 ci(build): support `aarch64-apple-darwin`

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -5,7 +5,7 @@ use wasm_bindgen::prelude::*;
 pub fn main_js() -> Result<(), JsValue> {
     // This provides better error messages in debug mode.
     // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
+    #[cfg(feature = "debug")]
     console_error_panic_hook::set_once();
 
     Ok(())
