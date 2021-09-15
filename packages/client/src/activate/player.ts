0d52e1cc648ac984ddc5b1d452da85a32a5e183a
YXL <chenxin.lan.76@gmail.com>
2021-09-15 14:51:39 +0800

    🐞 fix(native): add available modules

--- packages/client/src/activate/player.ts
+++ packages/client/src/activate/player.ts
@@ -4,14 +4,17 @@ import type { ExtensionContext } from "vscode";
 import { IPC } from "../utils";
 
 const available = [
+  "linux-arm.node",
+  "darwin-arm64.node",
+  "linux-arm64.node",
   "win32-arm64.node",
-  "darwin-x86.node",
-  "linux-x86.node",
-  "win32-x86.node",
+  "darwin-x64.node",
+  "linux-x64.node",
+  "win32-x64.node",
 ];
 
 export function initPlayer(context: ExtensionContext): void {
-  const name = `${platform()}-${arch() === "x64" ? "x86" : arch()}.node`;
+  const name = `${platform()}-${arch()}.node`;
   const wasm = PLAYER_MODE === "wasm" || !available.includes(name);
   console.log("Cloudmusic:", PLAYER_MODE, "mode.");
   IPC.init(context.globalState.get(VOLUME_KEY, 85), { wasm, name });
