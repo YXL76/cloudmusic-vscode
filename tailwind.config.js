71fd210875a4339e1957974ea64bb213547c1e57
兰陈昕 <43405544+YXL76@users.noreply.github.com>
2022-07-09 00:25:18 +0800

    🐳 chore(renovate): Update config (#745)
    Signed-off-by: YXL <chenxin.lan.76@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,10 @@
 
 /** @type {import("tailwindcss/tailwind-config").TailwindConfig } */
 const config = {
-  // @ts-ignore
   corePlugins: { preflight: false },
   darkMode: "class",
   content: ["./packages/webview/src/**/*.tsx"],
-  theme: {
-    extend: {
-      // @ts-ignore
-      zIndex: { "-10": -10 },
-    },
-  },
+  theme: { extend: { zIndex: { "-10": "-10" } } },
 };
 
 module.exports = config;
